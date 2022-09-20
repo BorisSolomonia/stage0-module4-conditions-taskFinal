6e249548e8471dfccb56f3251926f87f54d8e89c
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-09-20 11:00:43 +0400

    Update BitwiseValuesSwap.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
+        first = second^first;
+        second = first^second;
+        first = second^first;
+        System.out.println(first);
+        System.out.println(second);
     }
 }
